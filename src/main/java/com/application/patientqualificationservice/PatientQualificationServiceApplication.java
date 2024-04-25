3350d9b2831e4b4e63d24cfc5704afca2b60887b
imexh <chandikaranawella@gmail.com>
2024-04-25 17:37:36 +0530

    first commit

---
+++ src/main/java/com/application/patientqualificationservice/PatientQualificationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.application.patientqualificationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientQualificationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientQualificationServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/application/patientqualificationservice/PatientQualificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.patientqualificationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientQualificationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
